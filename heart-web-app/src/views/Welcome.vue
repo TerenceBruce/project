8a442977fd13dcb77ad66b1bc14ae8d747c03135
tomelkins <56346306+tomelkins@users.noreply.github.com>
2022-02-10 14:20:43 +0000

    Update Welcome.vue

--- heart-web-app/src/views/Welcome.vue
+++ heart-web-app/src/views/Welcome.vue
@@ -3,6 +3,10 @@
     <div v-if="loggedIn">Logged in as <b>{{ email }}</b><br><br>
         <el-button type="warning" @click="changePassword">Change Password</el-button>
         <el-button type="danger" @click="signOut">Sign out</el-button>
+        <br><br>
+        <div class="pageBody">
+            ALL DATA GOES IN THIS DIV
+        </div>
     </div>
     <div v-else>Not logged in.</div>
   </div>
@@ -59,5 +63,10 @@ export default {
 </script>
 
 <style>
+
+    .pageBody {
+        background: linear-gradient(360deg, #001E3C 0%, #002663 93.8%);
+        height: 80vh;
+    }
 
 </style>
